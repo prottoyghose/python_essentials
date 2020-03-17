de6a6d261b64d1da07ac637422d648a0dfd4e9ee
Prottoy Ghose <prottoyghose@gmail.com>
2020-03-17 18:53:13 -0400

    Finished till Chapter 4 - Lesson 1

--- Ch3/calendars_start.py
+++ Ch3/calendars_start.py
@@ -3,20 +3,27 @@
 #
 
 # import the calendar module
-
+import calendar
 
 # create a plain text calendar
-
+c = calendar.TextCalendar(calendar.SUNDAY )
+# st = c.formatmonth(2020,4,0,0)
+# print(st)
 
 # create an HTML formatted calendar
-
+# hc = calendar.HTMLCalendar(calendar.MONDAY)
+# st = hc.formatmonth(2020,4)
+# print(st)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
-
+# for i in c.itermonthdays(2020,4):
+#     print(i)
   
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
+for name in calendar.month_name:
+    print(name)
 
 
 # Calculate days based on a rule: For example, consider
--- Ch3/dates_start.py
+++ Ch3/dates_start.py
@@ -2,23 +2,34 @@
 # Example file for working with date information
 #
 
+from datetime import date
+from datetime import time
+from datetime import datetime
+
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
+  today = date.today()
+  print("Today's date is ", today)
 
   # print out the date's individual components
-
+  print("Date components: ", today.day, today.month, today.year)
   
   # retrieve today's weekday (0=Monday, 6=Sunday)
+  print ("Today's weekday is: ", today.weekday())
+
 
   
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
+  today = datetime.now()
+  print ("The current date and time is: ", today)
 
 
   # Get the current time
+  currentTime = datetime.time(today)
+  print ("Current time is: ", currentTime)
 
 
   
